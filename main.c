fa772e5cfd434976a1f0e1a922a8a6b30161250c
SergioB17 <ie726673@teso.mx>
2022-08-23 17:52:01 -0500

    repasando commit

--- main.c
+++ main.c
@@ -2,8 +2,12 @@
 #include "MK64F12.h"
 
 #define SIM_5 (*((volatile uint32_t *) 0x40048038))
+#define PCR_PTB22 (*((volatile uint32_t *) 0x4004A058))
+#define GPIO_B_PDOR (*((volatile uint32_t *) 0x400FF040))
+#define GPIO_B_PDDR (*((volatile uint32_t *) 0x400FF054))
 
 int main(void) {
+
 
     while(1) {
 
